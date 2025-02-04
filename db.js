53c5b574b9077b5f8b440db4dc682befc884e0f1
TokeHK <toha7@videndjurs.net>
2025-02-04 15:00:49 +0100

    a

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-//const MONGO_URI = process.env.DB;
-const MONGO_URI = "mongodb+srv://test:kVoPm55JmzN17pV6@final-advanced-frontend.lerwh.mongodb.net/?retryWrites=true&w=majority"
+const MONGO_URI = process.env.DB;
 
   mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
